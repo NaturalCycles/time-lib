df441f7097450ba18e52157c57c97f51d31593cd
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2019-07-05 20:10:33 +0200

    fix: export Dayjs interface

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import { dayjs } from './dayjs.full'
+import { dayjs, Dayjs } from './dayjs.full'
 import { ms, since, TS_2018_06_21 } from './time.util'
 
 export {
   dayjs, // should be imported from time.util, cause it's an extended version
+  Dayjs,
   TS_2018_06_21,
   since,
   ms,
