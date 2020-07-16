b4ec35d6e80b7a79edfb1782baa9bfdb9694bca5
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2020-07-16 18:40:56 +0200

    test: extra isoWeekday(Monday) tests

--- src/plugin/isoWeekday.test.ts
+++ src/plugin/isoWeekday.test.ts
@@ -1,3 +1,4 @@
+import { expectResults } from '@naturalcycles/dev-lib/dist/testing'
 import { dayjs } from '../dayjs.full'
 
 test.each([
@@ -29,4 +30,23 @@ test.each([
 
 test('today', () => {
   console.log(dayjs().isoWeekday())
+})
+
+const dates = [
+  '2020-07-11', // sat
+  '2020-07-12', // sun
+  '2020-07-13', // mon
+  '2020-07-14',
+  '2020-07-15',
+  '2020-07-16',
+  '2020-07-17',
+  '2020-07-18',
+  '2020-07-19', // sun
+  '2020-07-20', // mon
+  '2020-07-21', // tue
+]
+
+test('isoWeekday(Monday)', () => {
+  // should set it to the correct Monday of SAME WEEK
+  expectResults(d => dayjs(d).isoWeekday(1).toISODate(), dates).toMatchSnapshot()
 })
